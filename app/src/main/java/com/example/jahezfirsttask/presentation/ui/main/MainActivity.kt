448770b2e54b52077c91ea8b5a49fe8a6a45105a
zainab ALjutayl <zainab.Aljutayl.dmm.k1@tuwaiq.edu.sa>
2022-03-25 19:00:51 +0300

    fix bug of press back in home page (RestaurantListFragment) ,,when user press back making app close and go back to privies page

--- app/src/main/java/com/example/jahezfirsttask/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/jahezfirsttask/presentation/ui/main/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.jahezfirsttask.presentation.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import com.example.jahezfirsttask.R
 import com.example.jahezfirsttask.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -10,6 +13,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,8 +21,18 @@ class MainActivity : AppCompatActivity() {
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(
             this, R.layout.activity_main)
 
-       // setContentView(R.layout.activity_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+         navController = navHostFragment.navController
+
+    }
 
 
+    // fix bug of press back in home page (RestaurantListFragment) ,,app close
+    // so instead of app go back to privies page ,, app close
+    override fun onBackPressed() {
+        when(navController.currentDestination?.id) {
+            R.id.restaurantListFragment -> finish()
+            else -> super.onBackPressed()
+        }
     }
 }
